c90cf4ca8f15f5d5666a178e9fde343711ff6cdc
petarvukcevic <petar.vukcevic2001@gmail.com>
2023-10-20 14:39:47 +0200

    init

---
+++ e-lib-backend/src/main/java/com/petarvukcevic/elib/ELibBackendApplication.java
@@ -0,0 +1,13 @@
+package com.petarvukcevic.elib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibBackendApplication.class, args);
+	}
+
+}
